39c0986eaa918aae7bed90459013792b5e8218d7
otisbrower <otisbrower@yahoo.com>
2016-08-25 13:49:35 -0500

    timed out on sphere

--- SphereCollatz.py
+++ SphereCollatz.py
@@ -10,7 +10,6 @@
 # collatz_read
 # ------------
 
-
 def collatz_read(s):
     """
     read two ints
@@ -77,3 +76,41 @@ def collatz_solve(r, w):
         i, j = collatz_read(s)
         v = collatz_eval(i, j)
         collatz_print(w, i, j, v)
+
+# -------
+# imports
+# -------
+
+import sys
+
+# ----
+# main
+# ----
+
+#f __name__ == "__main__":
+collatz_solve(sys.stdin, sys.stdout)
+
+""" #pragma: no cover
+% cat RunCollatz.in
+1 10
+100 200
+201 210
+900 1000
+
+
+
+% RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+
+
+% cat RunCollatz.out
+1 10 1
+100 200 1
+201 210 1
+900 1000 1
+
+
+
+% pydoc3 -w Collatz
+# That creates the file Collatz.html
+"""
