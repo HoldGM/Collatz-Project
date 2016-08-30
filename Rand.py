d2f689af25a316eb0a1fc49ace268fc4f5500729
otisbrower <otisbrower@yahoo.com>
2016-08-30 13:27:28 -0500

    working on cache integration fixed #11

--- Rand.py
+++ Rand.py
@@ -1,8 +1,6 @@
 import random
 
-x = random.randint(0, 9)
-
-for x in range(0,194):
-	x = random.randint(0, 1000000)
-	y = random.randint(0, 1000000)
-	print(str(x) + " " + str(y))
+x = 1
+while x < 501:
+	print(str(x))
+	x += 1
